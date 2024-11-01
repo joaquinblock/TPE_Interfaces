de5f89ee179afb55b1b72ac5038b511809fc1db5
Joaquin Block <joaquin.block@gmail.com>
2024-11-01 13:12:20 -0300

    se agrego pantalla para seleccion de juego

--- js/game.js
+++ js/game.js
@@ -1,33 +1,5 @@
-let stars = document.querySelectorAll(".star")
-let lastStarClicked = ''
+const gameSelection = document.querySelector('.game-selection');
 
-stars.forEach(s => {
-    s.addEventListener("click", setRating)
+document.getElementById('play').addEventListener('click', () => {
+    gameSelection.classList.remove('hidden');
 })
-
-function setRating(e) {
-    if (localStorage.getItem("session")) {
-        stars.forEach(s => {
-            s.src = "../iconos/logo_estrella_vacia.svg"
-        })
-    
-        if (lastStarClicked != e.target.id) {
-            let n = e.target.id.split("-")[1]
-
-            lastStarClicked = e.target.id
-        
-            for (let i = 0; i < n; i++) {
-                stars[i].src = "../iconos/logo_estrella_llena.svg"
-            }
-        } else {
-            lastStarClicked = ''
-        }
-
-    } else {
-        document.body.classList.add('blur')
-        signin_form.classList.remove('invisible')
-        signin_form.classList.add('visible')
-        profile_menu.classList.remove('visible')
-        profile_menu.classList.add('invisible')
-    }
-}
